96fad1ee64b39b4c781869aa5d11637666d01460
vml-arquivos <vilsonmarcio@gmail.com>
2026-05-15 08:55:40 -0300

    Update PricingSimulator.tsx

--- client/src/pages/PricingSimulator.tsx
+++ client/src/pages/PricingSimulator.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect, useMemo } from "react";
 import { trpc } from "@/lib/trpc";
 import { useLocation } from "wouter";
 import {
